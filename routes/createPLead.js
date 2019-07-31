2d8d38ab5a48a6ead23d3b26441edd03b9ee6a5d
Jitendra <jitendra@Jitendra.local>
2019-07-31 18:21:45 +0530

    added createRecord endpoint

--- routes/createPLead.js
+++ routes/createPLead.js
@@ -1,57 +1,84 @@
 var express = require('express');
 var router = express.Router();
 var jsforce = require('jsforce');
-var credsRouter = require('./verifysfdc');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-    //res.send(' Initiating Login process...');
-//   console.log(req.body.url);
-//   console.log(req.body.uName);
-//   console.log(req.body.pwd);
-/*var conn = new jsforce.Connection({
-  // you can change loginUrl to connect to sandbox or prerelease env.
-    loginUrl : req.body.url 
-    //'https://test.salesforce.com'
-    //'hariharan@cloudbyz.com.wcct.ctmsdev'
-    //'wcct@2019AMcPMvXy8zYcwlAFG1TxEPqX'
-});
-conn.login(req.body.uName, req.body.pwd, function(err, userInfo) {
-  if (err) {  
-    console.error(err);
-    //res.send(' Login failed...');
-    res.json({status: 400, message:"Login Failed"});
-  }
-  // Now you can get the access token and instance URL information.
-  // Save them to establish connection next time.
-  console.log(conn.accessToken);
-  console.log(conn.instanceUrl);
-  // logged in user property
-  console.log("User ID: " + userInfo.id);
-  console.log("Org ID: " + userInfo.organizationId);
-  res.json({status: "200", message : "Login success"});
-});*/
-console.log(router.get('conData'));
-res.json({con:'conn'});
-function createRecord(){
+function connectSfdc(req){
+  return new Promise((resolve,reject)=>{
+      console.log(req.body.url);
+      console.log(req.body.uName);
+      console.log(req.body.pwd);
+      var conn = new jsforce.Connection({
+          loginUrl : req.body.url 
+      });
+      conn.login(req.body.uName, req.body.pwd, function(err, userInfo) {
+        if (err) {  
+          console.error(err);
+          var resObj={};
+          resObj.status ='400';
+          resObj.con='error';
+          resolve(resObj);
+        }
+        else{
+          console.log(conn.accessToken);
+          console.log(conn.instanceUrl);
+          console.log("User ID: " + userInfo.id);
+          console.log("Org ID: " + userInfo.organizationId);
+          resolve();
+          var resObj={};
+          resObj.status ='200';
+          resObj.con=conn;
+          resolve(resObj);
+        }
+      });
+  })
+}
+function createRecord(conn,req){
     return new Promise((resolve,reject)=>{
-
-        
+      logger.debug('Object to insert..');
+      logger.debug(req);
+      logger.debug(req.objectToCreate);
+      logger.debug(req.objectJson);
+      conn.sobject("Account").create(req.objectJson, function(err, ret) {
+        if (err || !ret.success) 
+        { 
+          console.error(err, ret);
+          var resObj={};
+          resObj.status=400;
+          resObj.message='Record Creation Failed!';
+          reject(resObj);
+        }
+        else
+        {
+          console.log("Created record id : " + ret.id);
+          var resObj={};
+          resObj.recordId=ret.id;
+          resObj.status=200;
+          resObj.message='Record Created Successfully!';
+          resolve(resObj);
+        }
+      });
     })
 }
-
-function createRecord(){
-    return new Promise((resolve,reject)=>{
-
-    })
-}
-
 function main(){
     let verifyConn = connectSfdc(req)();
-    verifyConn.then((resolve,reject)=>{
+    verifyConn.then((response)=>{
         console.log('connection verified... ');
-        return createRecord();
-    });
+        logger.debug(response);
+        if(res.status='200'){
+          return createRecord(res.con,req);
+        }
+        else{
+          res.json({status: "400", message : "Salesforce connection Failed"});
+        }
+    })
+    .then((finalRes)=>{
+      logger.debug(finalRes);
+      res.json(finalRes);
+    })
+    .catch((error)=>{
+      res.json(error);
+    })
 }
 main();
 });
