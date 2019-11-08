bfc8fa624486b78343c975fad67448ea01f620e9
Jitendra <jitendra@Jitendra.local>
2019-11-08 09:14:05 +0530

    added account creation logic

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var credsRouter = require('./routes/verifysfdc');
+//var usersRouter = require('./routes/users');
+//var credsRouter = require('./routes/verifysfdc');
 var createPLRecRouter = require('./routes/createPLead');
 
 var app = express();
@@ -22,9 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/verify',credsRouter);
-app.use('/createRecord',createPLRecRouter);
+//app.use('/users', usersRouter);
+//app.use('/verify',credsRouter);
+app.use('/createlead',createPLRecRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/createPLead.js
+++ routes/createPLead.js
@@ -2,85 +2,80 @@ var express = require('express');
 var router = express.Router();
 var jsforce = require('jsforce');
 
-router.get('/', function(req, res, next) {
-function connectSfdc(req){
-  return new Promise((resolve,reject)=>{
-      console.log(req.body.url);
-      console.log(req.body.uName);
-      console.log(req.body.pwd);
-      var conn = new jsforce.Connection({
-          loginUrl : req.body.url 
+router.post('/', function(req, res, next) {
+  logger.debug('data---');
+  logger.debug(req.body);
+  logger.debug('conn');
+  logger.debug(conn);
+  let insertOPS = function insertLead(conn){
+      logger.debug('Inserting Lead now.!!!');
+      return new Promise(function(resolve, reject) {
+          // Single record creation
+          conn.sobject("Account").create(req.body, function(err, ret) {
+          if (err || !ret.success) 
+          { 
+              return console.error(err, ret); 
+          }
+          else{
+              console.log("Created record id : " + ret.id);
+          }
+          });
+      })
+  }
+  let sfdcConnFn =function callJSForce(tables){
+      logger.debug('Calling JSFORCE now.!!!');
+      return new Promise(function(resolve, reject) {
+          var conn = new jsforce.Connection({
+              // you can change loginUrl to connect to sandbox or prerelease env.
+              loginUrl : 'https://test.salesforce.com'
+              });
+              conn.login('hariharan@cloudbyz.com.wcct.ctmsdev', 'wcct@2019ZCWqz8UMHZ47EDkisVpMQW2bu', function(err, userInfo) {
+              if (err) { 
+                  var resp={
+                      con :'error',
+                      status:'400'
+                  };
+                  reject(resp);
+                  console.error(err); 
+              }
+              else{
+                  //logger.debug(conn.instanceUrl);
+                  logger.debug("User ID: " + userInfo.id);
+                  logger.debug("Org ID: " + userInfo.organizationId);
+                  var resp={
+                      con :conn,
+                      status:'200',
+                  };
+                  resolve(resp);
+              }//sucess conn else
+              });//conn login fn.
+      
+      })
+  }
+  function main() {
+      var con;
+      //var db =dynamodb;
+      let connectSFDC = sfdcConnFn();
+      var totalTables=[];
+      connectSFDC.then((result)=>{
+          logger.debug("#####connected to SFDC "+result.status);
+          logger.debug('resul--------t');
+          logger.debug(result);
+          //logger.debug(result);
+          return insertOPS(result.con);
+      })
+      .then((result)=>{
+          //logger.debug(result);
+          logger.debug('####insertOps called: ');
+          res.send(JSON.stringify({'Status': 'Lead created successfully!!','Response':'200'}));
+      })
+      .catch((error)=>{
+          logger.debug(error);
+          res.status(404).end();
       });
-      conn.login(req.body.uName, req.body.pwd, function(err, userInfo) {
-        if (err) {  
-          console.error(err);
-          var resObj={};
-          resObj.status ='400';
-          resObj.con='error';
-          resolve(resObj);
-        }
-        else{
-          console.log(conn.accessToken);
-          console.log(conn.instanceUrl);
-          console.log("User ID: " + userInfo.id);
-          console.log("Org ID: " + userInfo.organizationId);
-          resolve();
-          var resObj={};
-          resObj.status ='200';
-          resObj.con=conn;
-          resolve(resObj);
-        }
-      });
-  })
-}
-function createRecord(conn,req){
-    return new Promise((resolve,reject)=>{
-      logger.debug('Object to insert..');
-      logger.debug(req);
-      logger.debug(req.objectToCreate);
-      logger.debug(req.objectJson);
-      conn.sobject(req.objectToCreate).create(req.objectJson, function(err, ret) {
-        if (err || !ret.success) 
-        { 
-          console.error(err, ret);
-          var resObj={};
-          resObj.status=400;
-          resObj.message='Record Creation Failed!';
-          reject(resObj);
-        }
-        else
-        {
-          console.log("Created record id : " + ret.id);
-          var resObj={};
-          resObj.recordId=ret.id;
-          resObj.status=200;
-          resObj.message='Record Created Successfully!';
-          resolve(resObj);
-        }
-      });
-    })
-}
-function main(){
-    let verifyConn = connectSfdc(req)();
-    verifyConn.then((response)=>{
-        console.log('connection verified... ');
-        logger.debug(response);
-        if(res.status='200'){
-          return createRecord(res.con,req);
-        }
-        else{
-          res.json({status: "400", message : "Salesforce connection Failed"});
-        }
-    })
-    .then((finalRes)=>{
-      logger.debug(finalRes);
-      res.json(finalRes);
-    })
-    .catch((error)=>{
-      res.json(error);
-    })
-}
-main();
+  };
+  //starting the Event loop execution.
+  main();
 });
 
 module.exports = router;
